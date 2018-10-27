b8236211b5fc77de277adf7aef1b4d8a89b68651
molof <michaeloloughlint@gmail.com>
2018-10-27 03:12:29 -0400

    Added register form

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Squared BJJ' });
 });
 
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Squared BJJ' });
+});
 module.exports = router;
