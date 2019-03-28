47d09eee2055ddab40bc8d6a1c86c45e8d55e64d
molof <michaeloloughlint@gmail.com>
2019-03-28 09:25:01 -0400

    DB CONNECTION ESTABLISHED FUCKING FINALLY

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,40 @@
 var express = require('express');
 var router = express.Router();
-/*const Pool = require('pg');*/
-
-
-const {Client} = require('pg');
+const sql = require('mssql');
 
 /*Try DB connect*/  
-const client = new Client({
-  user: "mike",
-  password: "poopmike",
-  host: "Michaels-MacBook-Air.local",
-  port: 5432,
-  database: "squared"
-})
-client.connect()
-.then(() => console.log("Connected to DB successfully!"))
-.catch(e => console.log)
-/* potential trash */
-.then(() => console.log(client.query("SELECTNOW()")))
-.finally(() => client.end())
+var dbConfig = {
+	server: "den1.mssql8.gear.host",
+	database: "squared",
+	user: "squared",
+	password: "Go6i4jfnaX!_",
+	port: 1433
+}
+
+/*
+async () => {
+	try {
+		await sql.connect(dbConfig)
+		const result = await sql.query('SELECT * FROM info')
+	}
+}
+*/
+function getInfo(){
+	var conn = new sql.ConnectionPool(dbConfig);
+	var req = new sql.Request(conn);
+
+	conn.connect(function (err) {
+		if (err){
+			console.log(err);
+			return;
+		}
+		req.query("SELECT * FROM info");
+		console.log("Success!")
+	});
+}
+
+getInfo();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
